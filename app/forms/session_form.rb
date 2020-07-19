cbac65671b3d47447d2657b8e35d445609b69ca4
M.Tamura <tamura2004@gmail.com>
2020-07-19 16:09:40 +0900

    Add i18n to login validation

--- app/forms/session_form.rb
+++ app/forms/session_form.rb
@@ -1,28 +1,31 @@
 class SessionForm
   include ActiveModel::Model
-  attr_accessor :id_name, :password
+  include ActiveModel::Attributes
 
-  validates :id_name, presence: { message: "ユーザーIDを入力してください" }
+  attribute :id_name, :string
+  attribute :password, :string
+
+  validates :id_name, :password, presence: { message: "%{attribute}を入力してください" }
   validates :id_name, format: { with: /\A[a-z]+\z/, message: "英子文字のみが使えます" }
   validates :id_name, length: { maximum: 6, too_long: "最大6文字です" }
-  validates :password, presence: { message: "パスワードを入力してください" }
   validates :password, format: { with: /\A[0-9]{4}\z/, message: "パスワードは数字4文字です" }
+ 
   validate :user_presence
   validate :password_valid
 
   def user_presence
-    unless user
+    if !user
       errors.add(:id_name, "存在しないユーザーです")
     end
   end
   
   def password_valid
-    unless password.to_i ^ 1234 == user&.masked_password
+    if password.to_i ^ 1234 != user&.masked_password
       errors.add(:password, "パスワードが違います")
     end
   end
-  
+
   def user
-    @user ||= User.where(id_name: id_name).sample
+    User.find_by(id_name: id_name)
   end
 end
