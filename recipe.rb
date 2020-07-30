797c81aa00ce774fedefe8ebb7b514ad267c3182
M.Tamura <tamura2004@gmail.com>
2020-07-30 16:29:04 +0900

    Fix recipe

--- recipe.rb
+++ recipe.rb
@@ -1,4 +1,6 @@
 packages = [
+  "sudo",
+  "wget",
   "build-essential",
   "sqlite3",
   "libsqlite3-dev",
@@ -6,25 +8,54 @@ packages = [
 ]
 
 packages.each do |name|
+  user "root"
   package name
 end
 
-directory "/home/moneta/src"
-
-execute "download and extruct chruby" do
-  command "cd /home/moneta/src"
-  command "wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz"
-  command "tar -xzvf chruby-0.3.9.tar.gz"
-  command "cd chruby-0.3.9"
-  command "make install"
-  command "echo 'source /usr/local/share/chruby/chruby.sh' >> /home/moneta/.bashrc"
-  command "echo 'source /usr/local/share/chruby/auto.sh' >> /home/moneta/.bashrc"
+directory "src" do
+  user "moneta"
+  cwd "/home/moneta"
+  mode "755"
+  owner "moneta"
+  group "moneta"
 end
 
-execute "download and extruct ruby-install" do
-  command "cd /home/moneta/src"
-  command "wget -O ruby-install-0.7.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.1.tar.gz"
-  command "tar -xzvf ruby-install-0.7.1.tar.gz"
-  command "cd ruby-install-0.7.1"
-  command "make install"
+[
+  "wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz",
+  "tar -xzvf chruby-0.3.9.tar.gz",
+].each do |cmd|
+  execute cmd do
+    user "moneta"
+    cwd "/home/moneta/src"
+  end
+end
+
+execute "make install" do
+  user "root"
+  cwd "/home/moneta/src/chruby-0.3.9"
+end
+
+[
+  "echo 'source /usr/local/share/chruby/chruby.sh' >> .bashrc",
+  "echo 'source /usr/local/share/chruby/auto.sh' >> .bashrc",
+].each do |cmd|
+  execute cmd do
+    user "moneta"
+    cwd "/home/moneta"
+  end
+end
+
+[
+  "wget -O ruby-install-0.7.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.1.tar.gz",
+  "tar -xzvf ruby-install-0.7.1.tar.gz",
+].each do |cmd|
+  execute cmd do
+    user "moneta"
+    cwd "/home/moneta/src"
+  end
+end
+
+execute "make install" do
+  user "root"
+  cwd "/home/moneta/src/ruby-install-0.7.1"
 end
