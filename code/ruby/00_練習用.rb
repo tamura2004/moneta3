f28fc5c0061a4dcab6b09e58aae0e765654465aa
M.Tamura <tamura2004@gmail.com>
2020-08-11 20:13:49 +0900

    Update database.yml for postgresql

--- code/ruby/00_練習用.rb
+++ code/ruby/00_練習用.rb
@@ -1,9 +1,1 @@
-require "pathname"
-dir = "code/ruby/"
-file = Pathname.new("01_文字列.rb")
-num, name = file.basename.to_s.split(/_/)
-num = num.to_i
-
-10.upto(11) do |i|
-  `cp #{dir}#{file.to_s} #{dir}#{num}.#{i}_#{name}.rb`
-end
+puts "hello world"
